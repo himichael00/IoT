b71573018367b380c53a81f10444d06802ab4798
michael <michaelrio89@gmail.com>
2024-09-25 21:55:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function setMainRoomLed() {
+    ledMainroom.style.backgroundColor = "Red";
+    mainroomLedImage.src = "./assets/led-on.png";
+}function setBedRoomLed() {
+    ledBedroom.style.backgroundColor = "Red";
+    bedroomLedImage.src = "./assets/led-on.png";
+}function setDeskSetupLed() {
+    ledDesksetup.style.backgroundColor = "Red";
+    desksetupLedImage.src = "./assets/led-on.png";
+}function setCornerPlaceLed() {
+    ledCornerplace.style.backgroundColor = "Red";
+    cornerplaceLedImage.src = "./assets/led-on.png";
+}
