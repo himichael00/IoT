7926a4e04a4ff26b4ee065251dc27d794a5cd2b0
michael <michaelrio89@gmail.com>
2024-09-28 20:50:17 +0700

    add getter setter API and change UI

--- main.js
+++ main.js
@@ -1,13 +1,70 @@
+const endpoint = "http://192.168.1.28";
+
+function getMainRoomLed() {
+    fetch(endpoint + "/mainroom", {
+        method:"GET"
+    }).then(response => response.text()).then(result => {
+        if (result == "ON") {
+            ledMainroom.style.backgroundColor = "#FFDA1A";
+            mainroomLedImage.src = "./assets/led-on.png";
+        } else{
+            ledMainroom.style.backgroundColor = "#0051BA";
+            mainroomLedImage.src = "./assets/led-off.png";
+        }
+    });
+}
+
+function getBedRoomLed() {
+    fetch(endpoint + "/bedroom", {
+        method:"GET"
+    }).then(response => response.text()).then(result => {
+        if (result == "ON") {
+            ledBedroom.style.backgroundColor = "#FFDA1A";
+            bedroomLedImage.src = "./assets/led-on.png";
+        } else{
+            ledBedroom.style.backgroundColor = "#0051BA";
+            bedroomLedImage.src = "./assets/led-off.png";
+        }
+    });
+}
+
+function getDeskSetupLed() {
+    fetch(endpoint + "/desksetuproom", {
+        method:"GET"
+    }).then(response => response.text()).then(result => {
+        if (result == "ON") {
+            ledDesksetup.style.backgroundColor = "#FFDA1A";
+            desksetupLedImage.src = "./assets/led-on.png";
+        } else{
+            ledDesksetup.style.backgroundColor = "#0051BA";
+            desksetupLedImage.src = "./assets/led-off.png";
+        }
+    });
+}
+
 function setMainRoomLed() {
-    ledMainroom.style.backgroundColor = "Red";
-    mainroomLedImage.src = "./assets/led-on.png";
-}function setBedRoomLed() {
-    ledBedroom.style.backgroundColor = "Red";
-    bedroomLedImage.src = "./assets/led-on.png";
-}function setDeskSetupLed() {
-    ledDesksetup.style.backgroundColor = "Red";
-    desksetupLedImage.src = "./assets/led-on.png";
-}function setCornerPlaceLed() {
-    ledCornerplace.style.backgroundColor = "Red";
+    fetch(endpoint + "/mainroom", {
+        method:"POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+function setBedRoomLed() {
+    fetch(endpoint + "/bedroom", {
+        method:"POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+function setDeskSetupLed() {
+    fetch(endpoint + "/desksetuproom", {
+        method:"POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+function setCornerPlaceLed() {
+    ledCornerplace.style.backgroundColor = "#FFDA1A";
     cornerplaceLedImage.src = "./assets/led-on.png";
 }
+
+getMainRoomLed();
+getBedRoomLed();
+getDeskSetupLed();
